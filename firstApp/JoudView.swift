27b676e785168fb07b3b95db4588e4d39250adfd
joud <joudsba@gmail.com>
2025-09-29 11:31:26 +0300

    joud commit

--- firstApp/JoudView.swift
+++ firstApp/JoudView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct JoudView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("Unknown-1")
+                .resizable()
+                .frame(width:250, height:250)
+                .clipShape(RoundedRectangle(cornerRadius: 150))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 150) // Must match clipShape's cornerRadius
+                                .stroke(Color.yellow, lineWidth: 4) // Customize stroke color and width
+                        )
+            
+                
+            VStack(alignment: .leading, spacing: 10){
+                Text("Joud Almashgari")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundStyle(.yellow)
+                
+                Text("Art lover, baker by heart, a mother of 7 cats🐈‍⬛🐈‍⬛")
+                    .font(.title3)
+                    .fontDesign(.monospaced)
+                    .fontWeight(.thin)
+                    .foregroundStyle(.black)
+            }
+                
+        }
+        .padding()
     }
 }
 

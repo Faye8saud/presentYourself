9635141dffef593ce254a7bb75c4b62f8b1a0009
joud <joudsba@gmail.com>
2025-09-29 11:27:28 +0300

    first commit

---
+++ firstApp/JoudView.swift
@@ -0,0 +1,18 @@
+//
+//  JoudView.swift
+//  firstApp
+//
+//  Created by Joud Almashgari on 29/09/2025.
+//
+
+import SwiftUI
+
+struct JoudView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JoudView()
+}
